6dc917c99d05cf4b3110ac1e61262562631109e5
Cécile Dubois <duboiscecilepro@gmail.com>
2021-03-19 09:34:24 +0100

    added sites.html / gastronomie.html / english traduction

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const i18n = domI18n({
-  selector: '[data-translatable]',
-  separator: ' // ',
-  languages: ['en', 'fr'],
-  defaultLanguage: 'fr',
+  selector: "[data-translatable]",
+  separator: " // ",
+  languages: ["en", "fr"],
+  defaultLanguage: "fr",
 });
 
-document.getElementById('switch-fr-btn').addEventListener('click', () => {
-  i18n.changeLanguage('en');
+const switchBtn = document.getElementById("switch_btn");
+switchBtn.addEventListener("click", () => {
+  switch (switchBtn.className) {
+    case "switch-to-en-btn":
+      i18n.changeLanguage("en");
+      switchBtn.className = "switch-to-fr-btn";
+      switchBtn.src = "assets/images/UK_flag.png";
+      break;
+    case "switch-to-fr-btn":
+      i18n.changeLanguage("fr");
+      switchBtn.className = "switch-to-en-btn";
+      switchBtn.src = "assets/images/France_flag.png";
+      break;
+    default:
+      break;
+  }
 });
-document.getElementById('switch-en-btn').addEventListener('click', () => {
-  i18n.changeLanguage('fr');
-});
+
+// document.getElementById('switch-fr-btn').addEventListener('click', () => {
+//   i18n.changeLanguage('en');
+// });
+// document.getElementById('switch-en-btn').addEventListener('click', () => {
+//   i18n.changeLanguage('fr');
+// });

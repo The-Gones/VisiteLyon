340789372e29abaa8f54522942f476e897aa18b5
Benoit <benoitredondo@gmail.com>
2021-03-24 14:56:36 +0100

    corrected spelling mistakes

--- gastronomie.js
+++ gastronomie.js
@@ -5,28 +5,28 @@ const sitesList = [
       name: "Restaurant Gastronomique Christian Tetedoie",
       district: "Lyon 5ème // Lyon 5th",
       picture: "./assets/images/restaurants/restaurant_tetedoie.jpg",
-      description: "<span>Assurement une des plus belles tables de Lyon.Le restaurant de Christian Christian Têtedoie nous propose une cuisine gastronomique prestigieuse.</span> <span>Certainly one of the most beautiful restaurants in Lyon. Christian Têtedoie\'s restaurant offers prestigious gourmet cuisine.</span>",
+      description: "<span>Assurement une des plus belles tables de Lyon.Le restaurant de Christian Têtedoie nous propose une cuisine gastronomique prestigieuse.</span> <span>Certainly one of the most beautiful restaurants in Lyon. Christian Têtedoie\'s restaurant offers prestigious gourmet cuisine.</span>",
     },
     {
       id:2,
       name: "Restaurant Paul Bocuse",
       district: "Collonges au Mont d'Or // Collonges Collonges au Mont d'Or ",
       picture: "./assets/images/restaurants/restaurant_paul_bocuse.png",
-      description: "<span>Commemt decrire un monumemt tel que Monsieur Paul ? Sa disparition il y a 2 ans a laisse un grand vide a Collonges mais le flambeau a vite ete repris et la famille reste aux commandes</span><span>How to describe a monument such as Monsieur Paul? His disappearance 2 years ago left a great void in Collonges but the torch was quickly picked up and the family remains in charge</span>"
+      description: "<span>Comment dérire un monumemt tel que Monsieur Paul ? Sa disparition il y a 2 ans a laissé un grand vide a Collonges mais le flambeau a vite été repris et la famille reste aux commandes.</span><span>How to describe a monument such as Monsieur Paul? His disappearance 2 years ago left a great void in Collonges but the torch was quickly picked up and the family remains in charge</span>"
     },
     {
       id:3,
       name: "Restaurant La Table de Max",
       district: "Lyon 7ème // Lyon 7th",
       picture: "./assets/images/restaurants/restaurant_la_table_de_max.jpg",
-      description: "<span>Un restaurant de copain pour une cuisine simple mais recomfortante</span><span>A friend's restaurant for simple but comforting cuisine</span>"
+      description: "<span>Un restaurant de copain pour une cuisine simple mais recomfortante.</span><span>A friend's restaurant for simple but comforting cuisine</span>"
     },
     {
       id:4,
       name: "Bouchon Les Lyonnais",
       district: "Lyon 5er // Lyon 5st",
       picture: "./assets/images/restaurants/restaurant_bouchon_les_lyonnais.JPG",
-      description: "<span>Ahhhh les celebres bouchons lyonnais, si tu as 50 ans et que tu n'as pas mis les pieds dans un bouchon c'est que tu as rate ta vie !</span><span>Ahhhh the famous Bouchons Lyonnais, if you are 50 years old and you have not set foot in a cork you have missed your life!</span>"
+      description: "<span>Ahhhh les celebres bouchons lyonnais, si tu as 50 ans et que tu n'as pas mis les pieds dans un bouchon c'est que tu as raté ta vie !</span><span>Ahhhh the famous Bouchons Lyonnais, if you are 50 years old and you have not been in a Bouchon Lyonnais you have missed part of your life!</span>"
     },
   ];
 

6ea56e2ec45d5e95ebd282c343e5379bbfde551c
Benoit <benoitredondo@gmail.com>
2021-03-24 13:35:48 +0100

    add Restaurants data base, pics and texts

--- gastronomie.js
+++ gastronomie.js
@@ -3,29 +3,29 @@ const sitesList = [
       id: 1,
       name: "Restaurant Gastronomique Christian Tetedoie",
       district: "Lyon 5ème // Lyon 5th",
-      picture: "./assets/images/restaurants/restaurant_tetedoie.jpg"
-      description: "<span>Le saucisson brioché ou saucisson en brioche est une spécialité culinaire de charcuterie pâtissière traditionnelle de la cuisine lyonnaise, à base de saucisson chaud lyonnais cuit à l'étouffée au four dans une pâte à brioche moelleuse dorée, et servi en tranche1. </span>"
+      picture: "./assets/images/restaurants/restaurant_tetedoie.jpg",
+      description: "<span>Assurement une des plus belles tables de Lyon.Le restaurant de Christian Christian Têtedoie nous propose une cuisine gastronomique prestigieuse.</span> <span>Certainly one of the most beautiful restaurants in Lyon. Christian Têtedoie\'s restaurant offers prestigious gourmet cuisine.</span>",
     },
     {
       id:2,
       name: "Restaurant Paul Bocuse",
       district: "Collonges au Mont d'Or // Collonges Collonges au Mont d'Or ",
-      picture: "./assets/images/Lyon-5eme.png",
-      description: "<span>Blabla français 2</span><span>Blabla english 2</span>"
+      picture: "./assets/images/restaurants/restaurant_paul_bocuse.png",
+      description: "<span>Commemt decrire un monumemt tel que Monsieur Paul ? Sa disparition il y a 2 ans a laisse un grand vide a Collonges mais le flambeau a vite ete repris et la famille reste aux commandes</span><span>How to describe a monument such as Monsieur Paul? His disappearance 2 years ago left a great void in Collonges but the torch was quickly picked up and the family remains in charge</span>"
     },
     {
       id:3,
-      name: "Restaurant la Table de Max",
+      name: "Restaurant La Table de Max",
       district: "Lyon 7ème // Lyon 7th",
-      picture: "./assets/images/lyon_6eme-Parc_de_la_Tête_d_Or.jpg",
-      description: "<span>Blabla français 3</span><span>Blabla english 3</span>"
+      picture: "./assets/images/restaurants/restaurant_la_table_de_max.jpg",
+      description: "<span>Un restaurant de copain pour une cuisine simple mais recomfortante</span><span>A friend's restaurant for simple but comforting cuisine</span>"
     },
     {
       id:4,
       name: "Bouchon Les Lyonnais",
       district: "Lyon 5er // Lyon 5st",
-      picture: "./assets/images/roue-bellecour.jpg",
-      description: "<span>Blabla français 4</span><span>Blabla english 4</span>"
+      picture: "./assets/images/restaurants/rrestaurant_bouchon_les_lyonnais.JPG",
+      description: "<span>Ahhhh les celebres bouchons lyonnais, si tu as 50 ans et que tu n'as pas mis les pieds dans un bouchon c'est que tu as rate ta vie !</span><span>Ahhhh the famous Bouchons Lyonnais, if you are 50 years old and you have not set foot in a cork you have missed your life!</span>"
     },
   ];
   

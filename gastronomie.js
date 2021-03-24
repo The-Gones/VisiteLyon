4b59b15182cd180793d098af39844bd09ae51a23
Benoit <benoitredondo@gmail.com>
2021-03-24 11:28:30 +0100

    add gastronomie.js file

---
+++ gastronomie.js
@@ -0,0 +1,31 @@
+const sitesList = [
+    {
+      id: 1,
+      name: "Restaurant Gastronomique Christian Tetedoie",
+      district: "Lyon 5ème // Lyon 5th",
+      picture: "./assets/images/roue-bellecour.jpg",
+      description: "<span>Le saucisson brioché ou saucisson en brioche est une spécialité culinaire de charcuterie pâtissière traditionnelle de la cuisine lyonnaise, à base de saucisson chaud lyonnais cuit à l'étouffée au four dans une pâte à brioche moelleuse dorée, et servi en tranche1. </span>"
+    },
+    {
+      id:2,
+      name: "Restaurant Paul Bocuse",
+      district: "Collonges au Mont d'Or // Collonges Collonges au Mont d'Or ",
+      picture: "./assets/images/Lyon-5eme.png",
+      description: "<span>Blabla français 2</span><span>Blabla english 2</span>"
+    },
+    {
+      id:3,
+      name: "Restaurant la Table de Max",
+      district: "Lyon 7ème // Lyon 7th",
+      picture: "./assets/images/lyon_6eme-Parc_de_la_Tête_d_Or.jpg",
+      description: "<span>Blabla français 3</span><span>Blabla english 3</span>"
+    },
+    {
+      id:4,
+      name: "Bouchon Les Lyonnais",
+      district: "Lyon 5er // Lyon 5st",
+      picture: "./assets/images/roue-bellecour.jpg",
+      description: "<span>Blabla français 4</span><span>Blabla english 4</span>"
+    },
+  ];
+  
